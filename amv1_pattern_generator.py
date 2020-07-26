4aa5d0ba15423a895c9564c7a5c8ef8af7f8d115
bennyBoy_JP <60102024+bennyBoyJP@users.noreply.github.com>
2020-07-26 21:29:14 +0900

    completed pattern combiner
    pattern combiner can now dynamically create array for printing

--- amv1_pattern_generator.py
+++ amv1_pattern_generator.py
@@ -1,16 +1,27 @@
-def pattern_generator(x, y):
+def pattern_generator(x, y, added):
     import random
     outer, inner = [],[]
 
-    for countRow in range(x):
-        for countColumn in range(int(y / 2)):
-            inner.append(random.randint(0, 1))
-        if y % 2 != 0:
-            inner.append(random.randint(0, 1))
-            inner.extend(inner[-2::-1])
-        else:
-            inner.extend(inner[-1::-1])
-        outer.append(inner)
-        inner = []
+    if added:
+        for countRow in range(x):
+            for countColumn in range(y):
+                inner.append(0)
+            outer.append(inner)
+            inner = []
 
-    return outer
+        return outer
+
+    else:
+        for countRow in range(x):
+            for countColumn in range(int(y / 2)):
+                inner.append(random.randint(0, 1))
+            if y % 2 != 0:
+                inner.append(random.randint(0, 1))
+                inner.extend(inner[-2::-1])
+            else:
+                inner.extend(inner[-1::-1])
+            outer.append(inner)
+            inner = []
+
+        return outer
+
